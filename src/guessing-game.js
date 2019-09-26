f7719209d17bf6906f3f484722d632a9a1eb5093
9pasha <pavel.b820@gmail.com>
2019-09-26 18:30:26 +0300

    feat: feat complete

---
+++ src/guessing-game.js
@@ -0,0 +1,27 @@
+class GuessingGame {
+    constructor() {
+        this.num = 0;
+        this.min = 0;
+        this.max = 0;
+    }
+
+    setRange(min, max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    guess() {
+        this.num = Math.round((this.min + this.max) / 2);
+        return this.num;
+    }
+
+    lower() {
+        this.max = this.num;
+    }
+
+    greater() {
+        this.min = this.num;
+    }
+}
+
+module.exports = GuessingGame;
